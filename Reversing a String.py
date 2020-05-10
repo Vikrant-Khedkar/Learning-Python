e1cec2544462bf8ed5db777e3cebe369bda02926
Vikrant Khedkar <vikrantkhedkar2720@gmail.com>
2020-05-10 21:47:09 +0530

    Addded Reversing  a String program

---
+++ Reversing a String.py
@@ -0,0 +1,11 @@
+str = "y"
+while str!="n":
+
+
+   str1 = input("Enter a String")
+   str2 = str1[::-1]
+   print("Reverse of {} is {}".format(str1,str2))
+   print("............")
+   str = input("Do you want to continue /n Reply with'y' for yes and 'n' for no")
+
+input("")
